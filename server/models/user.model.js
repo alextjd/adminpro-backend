c66ad94c045745243792fb5ac5eaf9ed2bb99c8e
Alex <alextjd1@gmail.com>
2021-04-22 22:03:41 +0200

    Implement user schema

---
+++ server/models/user.model.js
@@ -0,0 +1,12 @@
+import { model, Schema } from "mongoose";
+
+const UserSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true, unique: true },
+  img: { type: String },
+  role: { type: String, required: true, default: "USER" },
+  google: { type: Boolean, default: false },
+});
+
+export default model("User", UserSchema);
